2531d5fde814e27e5d2267e4b0825537f7509d22
chenwenjie <chenwenjie.star@bytedance.com>
2021-06-13 13:45:25 +0800

    init

---
+++ lib/main.dart
@@ -0,0 +1,94 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(home: PositionedTiles()));
+}
+
+class PositionedTiles extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => PositionedTilesState();
+}
+
+class PositionedTilesState extends State<PositionedTiles> {
+  List<Widget> tiles;
+
+  @override
+  void initState() {
+    super.initState();
+    tiles = [
+      StatelessColorfulTile(),
+      StatelessColorfulTile(),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Row(
+          children: tiles,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.sentiment_very_satisfied),
+        // child: Icon(Icons.sentiment_very_dissatisfied),
+        onPressed: swapTiles,
+      ),
+    );
+  }
+
+  void swapTiles() {
+    setState(() {
+      tiles.insert(1, tiles.removeAt(0));
+    });
+  }
+}
+
+// ignore: must_be_immutable
+class StatelessColorfulTile extends StatelessWidget {
+  Color color = ColorUtil.randomColor();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: color,
+        child: Padding(padding: EdgeInsets.all(70.0))
+    );
+  }
+}
+
+class StatefulColorfulTile extends StatefulWidget {
+  StatefulColorfulTile({Key key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => StatefulColorfulTileState();
+}
+
+class StatefulColorfulTileState extends State<StatefulColorfulTile> {
+  Color color;
+
+  @override
+  void initState() {
+    super.initState();
+    color = ColorUtil.randomColor();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: color,
+        child: Padding(padding: EdgeInsets.all(70.0))
+    );
+  }
+}
+
+class ColorUtil {
+  static Color randomColor() {
+    var red = Random.secure().nextInt(255);
+    var greed = Random.secure().nextInt(255);
+    var blue = Random.secure().nextInt(255);
+    return Color.fromARGB(255, red, greed, blue);
+  }
+}
